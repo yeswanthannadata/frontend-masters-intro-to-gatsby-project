252af69c89109e9e008a2255d0317fb49877ee2b
Yeshu <yeswanth@flutura.com>
2022-06-29 23:56:48 +0530

    gastby config and hook to query data from graphql

---
+++ gatsby-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: 'http://ayeswanth.com',
+    title: 'FrontEnd Masters - Intro to Gatsby',
+    description: 'FrontEnd Masters - Intro to Gatsby Course',
+    image:
+      'https://res.cloudinary.com/jlengstorf/image/upload/v1628127675/frontend-masters/gatsby-intro/share-image.jpg',
+  },
+};
