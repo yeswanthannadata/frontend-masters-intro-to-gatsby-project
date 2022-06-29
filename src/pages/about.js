3e6e5ac44dc0b42562c2764dba4628cb3a90636e
Yeshu <yeswanth@flutura.com>
2022-06-29 23:25:59 +0530

    add link to navigate between routes

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,11 @@
 import * as React from 'react';
+import { Link } from 'gatsby';
 
 export default function AboutPage() {
   return (
     <main>
       <h1>About this page</h1>
+      <Link to="/">back to home</Link>
     </main>
   );
 }
